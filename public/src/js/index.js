42dfb4c601ecaceb3582c16a89e463713a589669
datotoda <datotoda1342@gmail.com>
2023-05-11 16:04:38 +0400

    Improved responsivity

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -3,6 +3,7 @@ const API_KEY = "3fd2be6f0c70a2a598f084ddfb75487c"
 const API_MOVIES_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
 const API_SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
 const API_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w1280"
+const MOVIE_BASE_URL = "https://www.themoviedb.org/movie/"
 
 async function getData(url) {
     return fetch(url).then((response) => response.json()).catch(() => {
@@ -16,12 +17,14 @@ function getCardHtml(data) {
     let vote = data.vote_average
     let voteColorClass = vote >= 8 ? "t-c-high" : vote >= 4 ? "t-c-norm" : "t-c-low"
 
+    let linkTagOpen = `<a href="${MOVIE_BASE_URL + data.id}" target="__blank">`
     let titleTag = `<h3>${data.original_title}</h3>`
     let overviewTag = `<p>${data.overview}</p>`
     let voteTag = `<span class="${voteColorClass}">${vote}</span>`
     let imgageTag = `<img src="${API_IMAGE_BASE_URL + data.poster_path}" onerror="this.src='src/images/no_poster.jpg';">`
 
     cardHtml += `<div class="movie">`
+    cardHtml += linkTagOpen
     cardHtml += imgageTag
     cardHtml += `<div class="movie-front">`
     cardHtml += titleTag
@@ -31,11 +34,11 @@ function getCardHtml(data) {
     cardHtml += `<h3>Overview</h3>`
     cardHtml += overviewTag
     cardHtml += `</div>`
+    cardHtml += `</a>`
     cardHtml += `</div>`
 
     return cardHtml
 }
-
 
 async function search() {
     let search_input = document.getElementById('search_input')
@@ -43,7 +46,6 @@ async function search() {
     search_input.value = ""
     render(data.results)
 }
-
 
 async function initialLoad() {
     let data = await getData(API_MOVIES_URL)
@@ -57,5 +59,3 @@ async function render(results) {
         section.innerHTML += getCardHtml(results[i])
     }
 }
-
-
