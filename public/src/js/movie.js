2899e9e850a1da4c2e96cd1c4967ed867eac7bca
datotoda <datotoda1342@gmail.com>
2023-05-24 15:58:39 +0400

    Added seats

--- public/src/js/movie.js
+++ public/src/js/movie.js
@@ -1,9 +1,13 @@
+function setTotalCost(total_cost) {
+    document.getElementsByClassName("total-cost")[0].textContent = total_cost
+}
+
 function movieDetailRender() {
     const movieEl = document.querySelector("#movie")
 
     let vote = data.vote_average
     let voteColorClass = vote >= 8 ? "t-c-high" : vote >= 4 ? "t-c-norm" : "t-c-low"
-
+    let release_date = new Date(data.release_date);
 
     movieEl.innerHTML = `
     <div id="poster">
@@ -11,6 +15,7 @@ function movieDetailRender() {
     </div>
     <div id="movie_information">
         <h1>${data.original_title}</h1>
+        <span class="year">${release_date.getFullYear()}</span>
         <span class="rate ${voteColorClass}">${vote}</span>
         <h3>Overview</h3>
         <p>${data.overview}</p>
@@ -20,9 +25,10 @@ function movieDetailRender() {
 
 function relatedMoviesRender() {
     const moviesEl = document.querySelector("#movies")
-    movies.filter(movie => movie.id !== data.id)
+    const relatedMovies = movies.filter(movie => movie.id !== data.id)
     .sort(() => Math.random() - Math.random())
-    .slice(0, 4)
+    relatedMovies.push(data)
+    relatedMovies.slice(0, 4)
     .forEach(element => {
         const movieEl = document.createElement("div")
         movieEl.classList.add("related_movie")
@@ -41,9 +47,9 @@ function relatedMoviesRender() {
         `
         movieEl.addEventListener("click", () => {
             localStorage.setItem("movie", JSON.stringify(element));
+            window.scroll(100, 100);
             window.location.reload()
         })
-
         moviesEl.appendChild(movieEl)
     });
 }
@@ -55,3 +61,57 @@ const movies = JSON.parse(localStorage.getItem("movies"))
 setCoverImage(data.backdrop_path)
 movieDetailRender()
 relatedMoviesRender()
+document.querySelector("#seats .title").textContent = data.original_title
+
+if (data.backdrop_path) {
+    document.querySelector(".screen").style.background = `url("${API_IMAGE_BASE_URL + data.backdrop_path}") no-repeat center/cover`
+}
+
+document.querySelectorAll(".seat").forEach((seat, seat_index) => {
+    seat.addEventListener("click", () => { 
+        const cost = seat.classList.contains("vip-seat") ? 25 : 15
+        if (seat.classList.contains("desabled-seat")) { 
+            return
+        }
+        
+        if (seat.classList.contains("seat-selected")) {
+            totalCost -= cost
+            seat.classList.remove("seat-selected")
+            
+            let seats = JSON.parse(localStorage.getItem("seats"))
+            seats = seats.filter((s) => s !== seat_index).sort((a, b) => a - b)
+            localStorage.setItem("seats", JSON.stringify(seats))
+        } else {
+            totalCost += cost
+            seat.classList.add("seat-selected")
+            
+            let seats = JSON.parse(localStorage.getItem("seats"))
+            seats.push(seat_index)
+            seats.sort((a, b) => a - b)
+            localStorage.setItem("seats", JSON.stringify(seats))
+        }
+        setTotalCost(totalCost)
+    })
+})
+
+let totalCost = 0
+localStorage.setItem("seats", JSON.stringify([]))
+setTotalCost(totalCost)
+
+
+function goCheckout() {
+    let seats = JSON.parse(localStorage.getItem("seats"))
+    let tickets = []
+
+    seats.sort((a, b) => a - b).forEach((seat) => {
+        tickets.push({
+            row: (seat - seat % 10) / 10 + 1,
+            seat: seat % 10 + 1,
+            isVip: [23, 24, 25, 26, 33, 34, 35, 36].includes(seat) 
+        })
+    })
+    localStorage.setItem("tickets", JSON.stringify(tickets))
+    window.location = "checkout.html"
+}
+
+document.querySelector(".footer button").addEventListener("click", goCheckout)
